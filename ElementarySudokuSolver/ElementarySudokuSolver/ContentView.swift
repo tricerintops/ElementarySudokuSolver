c67a41705c8f1498a8e25ce5abc2661c0511ba38
Erin Hamalainen <erinvh620@gmail.com>
2021-06-03 18:08:52 +0100

    Add Xcode project

---
+++ ElementarySudokuSolver/ElementarySudokuSolver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElementarySudokuSolver
+//
+//  Created by Erin Hamalainen on 03/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
