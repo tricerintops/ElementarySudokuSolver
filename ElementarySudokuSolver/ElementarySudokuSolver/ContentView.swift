17d5d20249adc0a776f638bb67398a5b98f7412a
Erin Hamalainen <erinvh620@gmail.com>
2021-06-04 20:41:07 +0100

    Display a single nonet (ie 3 x 3 grid)

--- ElementarySudokuSolver/ElementarySudokuSolver/ContentView.swift
+++ ElementarySudokuSolver/ElementarySudokuSolver/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let data = (1...9).map { "Item \($0)" }
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+        LazyVGrid(
+            columns: columns,
+            alignment: .center,
+            spacing: 0
+        ) {
+            ForEach(data, id: \.self) { item in
+                HStack {
+                    Spacer()
+                    Text(item)
+                    Spacer()
+                }
+                .padding()
+                .border(Color.blue, width: 1)
+            }
+        }
+        .border(Color.red, width: 3)
     }
 }
 
